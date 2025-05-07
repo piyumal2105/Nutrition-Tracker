3ebbc18856edef97ad9010f4ba7486138b4073e2
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2025-05-07 15:30:04 +0530

    commit changes

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Set your desired port here (e.g., 5173 or 5174)
+    strictPort: true, // Ensures Vite fails if the port is already in use
+  },
+});
